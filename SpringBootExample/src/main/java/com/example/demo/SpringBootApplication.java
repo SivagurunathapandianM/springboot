ce1a215906dec31b44a8ac68011406093c105f4b
SivagurunathapandianM <msgnp451992@gmail.com>
2022-03-28 23:43:25 +0530

    extends Springbootservletcontianer

--- SpringBootExample/src/main/java/com/example/demo/SpringBootApplication.java
+++ SpringBootExample/src/main/java/com/example/demo/SpringBootApplication.java
@@ -1,16 +1,43 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
-public class SpringBootApplication {
+public class SpringBootApplication extends SpringBootServletInitializer{
+	
+	/**
+	   * The entry point of application.
+	   *
+	   * @param args the input arguments
+	   * ---------------------------------------------------------------
+	   * when application want to deploy in Servlet Container: Tomcat.jetty etc
+	   * extends SpringBootServletInitializer -> extending the main class will helps
+	   */
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(SpringBootApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+	
 	/*
 	 * -------------------------------------------------------------------------------------
 	 * SpringBoot Main Website:
 	 * https://spring.io/projects/spring-boot
 	 * -------------------------------------------------------------------------------------
+	 * Maven Resource Plugin Issue:
+	 * https://stackoverflow.com/questions/65910112/maven-clean-install-failed-to-execute-goal-org-apache-maven-pluginsmaven-resou
+	 * -------------------------------------------------------------------------------------
+	 * 
+	 * Springboot War deployment to tomcat server:
+	 * https://www.baeldung.com/spring-boot-war-tomcat-deploy
+	 * -------------------------------------------------------------------------------------
+	 * 
+	 * 
     */	
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApplication.class, args);
-	}
+	
 }
---
+++ SpringBootExample/src/main/java/com/example/demo/Tomcatcontroller.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Tomcatcontroller {
+
+	/*@GetMapping("/hello")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+          .mapToObj(i -> "Hello number " + i)
+          .collect(Collectors.toList());
+    }*/
+	
+	@RequestMapping(value = "/SpringbootExample1")
+	public String hello(){
+		return "Hello, Springboot world";
+	}
+	
+	@RequestMapping(value = "/")
+    public String helloTomcat() {
+      return "Hello World from Tomcat";
+    }
+}
